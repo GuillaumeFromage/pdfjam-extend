94c0448fcdafbf034c91ea55a9891db4d63d99b0
Guillaume Barbe <gbeaulieu@koumbit.org>
2017-03-05 19:01:42 -0500

    Fixed syntax, first run

--- pdfunbook.pl
+++ pdfunbook.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 
-usage() {
-  echo "$0 [--booked] [--split-horizontally] [--duplicated-vertically] [--verbose] --file=patate.pdf";
-  echo << EOF
+use Getopt::Long;
+
+sub usage() {
+  print "$0 [--booked] [--split-horizontally] [--duplicated-vertically] [--verbose] --file=patate.pdf";
+  print <<EOF
 --booked   rearrange the page as they were bound in signature (by default, signatures
            are assumed to be half of the document page)
 --split-{horizontally|vertically} the document is split (in half by default) on the
@@ -17,8 +19,10 @@ original order.
 
 Not all options specified are currently implemented.
            
-}
+
 EOF
+;
+  exit 0;
 }
 
 GetOptions ('booked' => \$booked, 'split-horizontally' => \$splitVert, 'duplicated-vertically' => \$dupVert, 'verbose' => \$verbose, 'file=s' => \$file) or usage();
@@ -27,17 +31,21 @@ if ($file eq '') {
   usage();
 }
 
+print "pdfinfo $file | grep Pages | cut -d ':' -f 2 | tr -d ' '";
+my $nbPage = `pdfinfo "$file" | grep Pages | cut -d ':' -f 2 | tr -d ' '`;
+
+
 #patate.pdf contient genre 2 pages par page, genre page 1 pi 2 sur la page 1, page 3 pi 4 sur la page 2
-pdfinfo patate.pdf
+#pdfinfo patate.pdf
 # ca vous donne le nombre de page ($NBPG) pi la taille ($SIZE)
-for i in $(seq 1 $NBPG) ; do pdfjoin brochure-anticap_final.pdf $i -o page$(echo $i*2-1 | bc).pdf; done
+#for i in $(seq 1 $NBPG) ; do pdfjoin brochure-anticap_final.pdf $i -o page$(echo $i*2-1 | bc).pdf; done
 
 
 
 # vv Ca, c'est si on rasterize, ca arrive qu'on aille pas le choix de prendre le pdf tel quel...
 
 # ca sépare toute les pages en pdf d'une page (de deux pages)
-for i in $(seq 1 $NBPG) ; do convert -density 300 page$(echo $i*2-1 | bc).pdf -units PixelsPerInch -resample 300 -crop $UNFUCKEDSIZE +repage +adjoin page$(echo $i*2-1 | bc)-%d.png; done
+#for i in $(seq 1 $NBPG) ; do convert -density 300 page$(echo $i*2-1 | bc).pdf -units PixelsPerInch -resample 300 -crop $UNFUCKEDSIZE +repage +adjoin page$(echo $i*2-1 | bc)-%d.png; done
 # ici, vous avez calculé $UNFUCKEDSIZE  à partir de la taille de la page 
 # (en pts, crisse de formats de vidange 'méricaine, c'est 1/72ème de pouce, 
 # unfuckedsize est en pixel, fake multiplié par 300 (parce qu'on a resamplé 
@@ -49,12 +57,12 @@ for i in $(seq 1 $NBPG) ; do convert -density 300 page$(echo $i*2-1 | bc).pdf -u
 # dernière slice de 3 pixel. Ca vaut la peine itou de checker si les deux 
 # morceaux que ca chie en png sont de la même taille en pixel, parce que ca
 # se peux que le deuxième est plus mince si le premier est plus gros.
-A=0; for i in $(ls -t *png) ; do A=$(echo $A+1 | bc); mv $i page-$(echo $NBPG-$A|bc).png ; done
+#A=0; for i in $(ls -t *png) ; do A=$(echo $A+1 | bc); mv $i page-$(echo $NBPG-$A|bc).png ; done
 # ca icitte, ca marche, mais c'est wizard en sale, en faisant ls -t, ca sort
 # les fichiers dans l'ordre qui se sont fait vomir par la commande précédente,
 # fake comme on itérait dans le bon ordre des pages, ca "marche" ; mais on
 # "inverse" l'ordre en commençant de la fin
-for i in $(ls -t page*png) ; do convert $i $(echo $(echo $i | cut -d '.' -f 1)).pdf ; done
+#for i in $(ls -t page*png) ; do convert $i $(echo $(echo $i | cut -d '.' -f 1)).pdf ; done
 # icitte on reconverti toute la shit en pdf
-A= ; for i in $(ls -t page-*pdf) ; do A=$A echo -n "$i " ; done ; pdfjoin $A
+#A= ; for i in $(ls -t page-*pdf) ; do A=$A echo -n "$i " ; done ; pdfjoin $A
 # pi on crisse tout ca d'un tapon !
