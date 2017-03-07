8eccd8f1d83115f188caf9bcf65f708762c01475
Guillaume Barbe <gbeaulieu@koumbit.org>
2017-03-06 21:13:27 -0500

    Got the single fold figured out.

--- gensig.pl
+++ gensig.pl
@@ -12,10 +12,15 @@ sub usage() {
   It assumes xerox config, eg: when landscape paper is used, it assumes that paper is
   flipped on the short side, hence, single fold signatures of a 4 pages zine is 4 1 2 3
   and not 4 1 2f 3f.
+
+  It uses B for blank pages. However, by default, it uses the number of page submitted
+  as the signature size, so you'll get an error instead of blank pages if you don't 
+  specify the signature size when you have a number of pages non-divisible by 2 to the
+  power of (the number of folds+1).
  
   It is designed to be called by software that does the work for you. Please contribute
   to this project, or ask me what the hell you're trying to do if you're that far down
-  the hole.
+  the hole. 
 
 EOF
 ;
@@ -24,7 +29,7 @@ EOF
 
 my $nbpg = 0;
 my $signature = 0;
-my $folds = 0;
+my $folds = 1;
 
 GetOptions ('nbpg=s' => \$nbpg, 'signature=s' => \$signature, 'folds=s' => \$folds  ) or usage();
 
@@ -38,8 +43,8 @@ if ($folds eq '') {
 }
 
 # yeah, we only do up to three folds
-if ($folds < 1 or $folds >= 4) {
-  print "this software only does single fold\n";
+if ($folds <= 0 or $folds >= 3) {
+  print "this software only does single or double fold (eg: 12, 16 and 16 oblong pages signature are not yet supported)\n";
   exit 1;
 }
 
@@ -48,5 +53,28 @@ if ($signature eq '') {
   $signature = $nbpg;
 }
 
-# we need to warn if the signature size do
+# we need to warn if the signature size doesn't match the # of folds
+# (we need to have multiple of 4 pages for single fold, multiple of
+# 8 for dual fold, etc)
+if ($signature % 2**($folds+1)) {
+  print "Signature size incorrect.\nMore help is available if you invoke this program without argument\n";
+  exit 2;
+}
+my $output = '';
+# for each spread 
+for (my $i = 0; $i < $nbpg/2**($folds+1) ; $i++) {
+  #print "spread $i\n";
+  # for each individual page in the spread
+  for (my $j = 0; $j < 2**($folds+1) ; $j++) {
+    #print "page $j\n";
+    if ($folds == 1) {
+      if ($j == 0 or $j == 3) {
+         $output .= $nbpg - $i * ($folds + 1) - $j % ($folds + 1). " ";
+      } else {
+         $output .= $j + $i * ($folds + 1) . " ";
+      }
+    }
+  }
+}
 
+print "$output\n";
